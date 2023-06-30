39ddbbb7fa7637efdc673ce7e5948a4bcfcc36e8
weslley41 <41weslley@gmail.com>
2023-06-30 13:19:54 -0300

    fix align on loading/error page

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -7,21 +7,23 @@ class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final randomNumber = Random().nextInt(10) + 1;
-    return SingleChildScrollView(
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/$randomNumber.png',
-              height: 250,
-            ),
-            const SizedBox(height: 30),
-            const CircularProgressIndicator(),
-            const SizedBox(height: 20),
-          ],
+    return Center(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/$randomNumber.png',
+                height: 250,
+              ),
+              const SizedBox(height: 30),
+              const CircularProgressIndicator(),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
